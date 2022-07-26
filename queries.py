3bdf54f8855ed3cc09140e330fb1718399a3927e
Jake Martin <jm917@ic.ac.uk>
2022-07-26 15:07:16 +0100

    Limited direct queries.

--- queries.py
+++ queries.py
@@ -1,2 +1,2 @@
-untagged = "SELECT uid, name, path FROM films WHERE NOT EXISTS (SELECT filmid FROM tagmap WHERE tagmap.filmid = films.uid) AND NOT EXISTS (SELECT filmid FROM varmap WHERE varmap.filmid = films.uid)"
-random_pics = "SELECT uid, name, path FROM pictures"
+untagged = "SELECT uid, name, path FROM films WHERE NOT EXISTS (SELECT filmid FROM tagmap WHERE tagmap.filmid = films.uid) AND NOT EXISTS (SELECT filmid FROM varmap WHERE varmap.filmid = films.uid) LIMIT 50"
+random_pics = "SELECT uid, name, path FROM pictures LIMIT 50 ORDER BY RANDOM()"
